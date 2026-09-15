712782b106f96ed3466a6144387bcb641b0b16e3
maisarahfarhana <maisarahfarhana8@gmail.com>
2026-09-15 13:10:57 +0800

    Connect Resend email service

--- api/send-email.js
+++ api/send-email.js
@@ -1,47 +1,39 @@
 import { Resend } from "resend";
 
+
 const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 export default async function handler(req, res) {
 
+
     if (req.method !== "POST") {
+
         return res.status(405).json({
             message: "Method not allowed"
         });
+
     }
 
-    const {
-        client_name,
-        client_phone,
-        project_scope,
-        design_request,
-        project_type,
-        estimated_total
-    } = req.body;
-
-
-    const emailContent = `
-        <h2>New Website Inquiry - AS TechSolution</h2>
-
-        <p><b>Name:</b> ${client_name}</p>
-
-        <p><b>Phone:</b> ${client_phone}</p>
-
-        <p><b>Project Type:</b> ${project_type}</p>
-
-        <p><b>Estimated Budget:</b> ${estimated_total}</p>
-
-        <p><b>Project Scope:</b><br>
-        ${project_scope}</p>
-
-        <p><b>Design Request:</b><br>
-        ${design_request}</p>
-    `;
 
 
     try {
 
-        const data = await resend.emails.send({
+
+        const {
+
+            client_name,
+            client_phone,
+            project_scope,
+            design_request,
+            project_type,
+            estimated_total
+
+        } = req.body;
+
+
+
+        const { data, error } = await resend.emails.send({
 
             from: "AS TechSolution <onboarding@resend.dev>",
 
@@ -49,26 +41,77 @@ export default async function handler(req, res) {
                 "astechsolution.my@gmail.com"
             ],
 
-            subject: "New Website Inquiry",
 
-            html: emailContent
+            subject: `New Website Inquiry - ${client_name}`,
+
+
+            html: `
+
+            <h2>New Website Inquiry - AS TechSolution</h2>
+
+            <p><b>Name:</b> ${client_name}</p>
+
+            <p><b>Phone:</b> ${client_phone}</p>
+
+            <p><b>Project Type:</b> ${project_type}</p>
+
+            <p><b>Estimated Budget:</b> ${estimated_total}</p>
+
+            <hr>
+
+            <p><b>Project Scope:</b></p>
+            <p>${project_scope}</p>
+
+
+            <p><b>Design Request:</b></p>
+            <p>${design_request}</p>
+
+            `
 
         });
+
+
+
+        if(error){
+
+            console.log(error);
+
+            return res.status(500).json({
+
+                success:false,
+                error:error.message
+
+            });
+
+        }
+
 
 
         return res.status(200).json({
-            success: true,
+
+            success:true,
+            message:"Email sent successfully",
             data
+
         });
 
 
-    } catch (error) {
+
+    } catch(error){
+
+
+        console.log(error);
+
 
         return res.status(500).json({
-            success: false,
-            error: error.message
+
+            success:false,
+            error:error.message
+
         });
 
+
     }
+
 
 }
